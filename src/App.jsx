a32eeea70481279c96970d8e12240ca7bd321e78
Alok Sharma <kingofkingsalok@gmail.com>
2025-03-21 17:50:41 +0530

    added writer why us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Abrassio from './pages/Abrassio';
-import InstagramScraper from './pages/InstagramScraper';
-import TwitterScraper from './pages/TwitterScraper';
-import LinkedinScraper from './pages/LinkedinScraper';
-import SignUp from './pages/Signup';
-import Login from './pages/Login';
-import PrivateRoute from './components/PrivateRoute';
-import YoutubeScraper from './pages/Youtube';
-import TwitchScraper from './pages/Twitch';
-import TikTokScraper from './pages/TiktokScrapper';
-import { ToastContainer } from 'react-toastify';
-import Homepage from './pages/Homepage';
-import ContentGenerator from './pages/ContentGenerator';
-import Dashboard from './pages/Dashboard';
-import Lawbot from './pages/lawbot';
+{
+  /* eslint-disable no-unused-vars */
+}
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Abrassio from "./pages/Abrassio";
+import InstagramScraper from "./pages/InstagramScraper";
+import TwitterScraper from "./pages/TwitterScraper";
+import LinkedinScraper from "./pages/LinkedinScraper";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import YoutubeScraper from "./pages/Youtube";
+import TwitchScraper from "./pages/Twitch";
+import TikTokScraper from "./pages/TiktokScrapper";
+import { ToastContainer } from "react-toastify";
+import Homepage from "./pages/Homepage";
+import ContentGenerator from "./pages/ContentGenerator";
+import Dashboard from "./pages/Dashboard";
+import Lawbot from "./pages/lawbot";
+import WhyUs from "./pages/WhyContentGenerator";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         {/* ToastContainer for global notifications */}
-       
 
         {/* Header */}
         <Header />
@@ -33,30 +36,118 @@ function App() {
           <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               {/* Public routes */}
-              <Route path="/" element={<  Homepage/>} />
+              <Route path="/" element={<Homepage />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/login" element={<Login />} />
 
               {/* Protected routes */}
-              <Route path="/home" element={<PrivateRoute><Homepage /></PrivateRoute>} />
-              <Route path="/abrassio" element={<PrivateRoute><Abrassio /></PrivateRoute>} />
-              <Route path="/instagram" element={<PrivateRoute><InstagramScraper /></PrivateRoute>} />
-              <Route path="/twitter" element={<PrivateRoute><TwitterScraper /></PrivateRoute>} />
-              <Route path="/linkedin" element={<PrivateRoute><LinkedinScraper /></PrivateRoute>} />
-              <Route path="/youtube" element={<PrivateRoute><YoutubeScraper /></PrivateRoute>} />
-              <Route path="/twitch" element={<PrivateRoute><TwitchScraper/></PrivateRoute>} />
-              <Route path="/tiktok" element={<PrivateRoute><TikTokScraper/></PrivateRoute>} />
-              <Route path="/socialspark" element={<PrivateRoute><ContentGenerator/></PrivateRoute>} />
-              <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
-              <Route path="/legalbot" element={<PrivateRoute><Lawbot/></PrivateRoute>} />
-
+              <Route
+                path="/home"
+                element={
+                  <PrivateRoute>
+                    <Homepage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/abrassio"
+                element={
+                  <PrivateRoute>
+                    <Abrassio />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/instagram"
+                element={
+                  <PrivateRoute>
+                    <InstagramScraper />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/twitter"
+                element={
+                  <PrivateRoute>
+                    <TwitterScraper />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/linkedin"
+                element={
+                  <PrivateRoute>
+                    <LinkedinScraper />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/youtube"
+                element={
+                  <PrivateRoute>
+                    <YoutubeScraper />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/twitch"
+                element={
+                  <PrivateRoute>
+                    <TwitchScraper />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/tiktok"
+                element={
+                  <PrivateRoute>
+                    <TikTokScraper />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/socialspark"
+                element={
+                  <PrivateRoute>
+                    <WhyUs />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/try-it"
+                element={
+                  <PrivateRoute>
+                    <ContentGenerator />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/dashboard"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/legalbot"
+                element={
+                  <PrivateRoute>
+                    <Lawbot />
+                  </PrivateRoute>
+                }
+              />
             </Routes>
           </Suspense>
         </main>
 
         {/* Footer */}
         <Footer />
-        <ToastContainer position="top-center" autoClose={3000} hideProgressBar />
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar
+        />
       </div>
     </Router>
   );
